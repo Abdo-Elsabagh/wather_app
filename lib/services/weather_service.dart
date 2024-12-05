c6f4b08a04db228cded2274111f48b01161ce711
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2024-12-05 08:08:29 +0200

    "Added Dio dependency and implemented WeatherService class with API call"

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,3 +1,15 @@
-class WeatherService{
+import 'package:dio/dio.dart';
+import 'package:wather_app/model/weather_model.dart';
 
+class WeatherService {
+  final Dio dio;
+
+  WeatherService(this.dio);
+  Future<WeatherModel> getWather({required cityName}) async {
+    Response response = await dio.get(
+        'https://api.weatherapi.com/v1/forecast.json?key=4be996dcada64284adb42603240312&q=$cityName&days=1');
+    WeatherModel weatherModel = WeatherModel.fromjson(response.data);
+
+    return weatherModel;
+  }
 }
