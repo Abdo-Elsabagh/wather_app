efb149e10df2d9a17f0eeaab799726eca23e352b
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2024-12-08 23:28:46 +0200

    `Added API key and base URL to WeatherService, refactored getWather method to handle API responses and errors.`

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -3,13 +3,20 @@ import 'package:wather_app/model/weather_model.dart';
 
 class WeatherService {
   final Dio dio;
-
+  final String apiKey = '4be996dcada64284adb42603240312';
+  final String baseUrl = 'https://api.weatherapi.com/v1';
   WeatherService(this.dio);
-  Future<WeatherModel> getWather({required cityName}) async {
-    Response response = await dio.get(
-        'https://api.weatherapi.com/v1/forecast.json?key=4be996dcada64284adb42603240312&q=$cityName&days=1');
-    WeatherModel weatherModel = WeatherModel.fromjson(response.data);
+  Future<WeatherModel?> getWather({required cityName}) async {
+    Response response =
+        await dio.get('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1');
+    if (response.statusCode == 200) {
+      WeatherModel weatherModel = WeatherModel.fromjson(response.data);
+    } else {
+      final String errormessage = response.data['error']['message'];
+      throw Exception(errormessage);
+    }
+    return null;
 
-    return weatherModel;
+    // return weatherModel;
   }
 }
